f6ad8abb4c048fd046a65e24c1519e21cea964e6
Sumit Jaiswal <sj0269950@gmail.com>
2026-09-11 14:45:38 +0530

    chore: add swagger-ui-dist type declaration for clean build

---
+++ src/types/swagger-ui-dist.d.ts
@@ -0,0 +1,13 @@
+declare module "swagger-ui-dist" {
+  export const SwaggerUIBundle: any;
+  export const SwaggerUIStandalonePreset: any;
+  export const absolutePath: any;
+  export const getAbsoluteFSPath: any;
+  const defaultExport: {
+    SwaggerUIBundle: any;
+    SwaggerUIStandalonePreset: any;
+    absolutePath: any;
+    getAbsoluteFSPath: any;
+  };
+  export default defaultExport;
+}
